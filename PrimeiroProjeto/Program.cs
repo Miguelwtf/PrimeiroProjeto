52d280a7e077eeebad2099704ae17e992c724e24
Miguel Kotowey <miguel.kotowey@hotmail.com>
2024-11-20 22:37:21 -0300

    Aula 01

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Musica musica1 = new Musica();
+musica1.nome = "Stairway to Heaven";
+musica1.artista = "Led Zeppelin";
+musica1.duracao = 400;
+musica1.disponivel = true;
+
+Musica musica2 = new Musica();
+musica2.nome = "Hells Bells";
+musica2.artista = "AC/DC";
+musica2.duracao = 350;
+musica2.disponivel = false;
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
